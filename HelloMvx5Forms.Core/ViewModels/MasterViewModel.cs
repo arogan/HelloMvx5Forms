6ca3850754335c1173e7ea398a31428d3cdf5dee
ARogan <arogan@gmail.com>
2017-06-02 16:01:23 -0500

    added instance count

--- HelloMvx5Forms.Core/ViewModels/MasterViewModel.cs
+++ HelloMvx5Forms.Core/ViewModels/MasterViewModel.cs
@@ -5,8 +5,21 @@ namespace HelloMvx5Forms.Core.ViewModels
 {
     public class MasterViewModel: MvxViewModel
     {
+        
+        
+        private static int instanceCount = 0;
+        public int InstanceCount
+        {
+            get { return instanceCount; }
+            set
+            {
+                instanceCount = value;
+                RaisePropertyChanged(() => InstanceCount);
+            }
+        }
         public MasterViewModel()
         {
+            InstanceCount++;
         }
     }
 }
